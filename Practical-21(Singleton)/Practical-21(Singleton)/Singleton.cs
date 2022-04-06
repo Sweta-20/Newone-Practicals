1a99e88b8474603b29c55f16b18c5326f2c8a6d4
Sweta-20 <swetajha2031@gmail.com>
2022-04-06 18:33:49 +0530

    2nd commit

---
+++ Practical-21(Singleton)/Practical-21(Singleton)/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_21_Singleton_
+{
+    public class Singleton
+    {
+        private static Singleton instance = new Singleton();
+        private Singleton() { }
+        public static Singleton GetInstance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
